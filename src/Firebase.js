81be1f57e555551e535275569ec39c0187d3098a
Arham <muhammadarham59@gmail.com>
2024-04-23 08:29:21 +0500

    New changes made

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const appId = import.meta.env.VITE_APP_ID;
+const messageSenderId = import.meta.env.VITE_MESSAGE_SENDER_ID;
 
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: "international-recruitment-app.firebaseapp.com",
   projectId: "international-recruitment-app",
   storageBucket: "international-recruitment-app.appspot.com",
-  messagingSenderId: "279755863522",
+  messagingSenderId: messageSenderId,
   appId: appId,
 };
 
